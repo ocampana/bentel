cf9eb6884a0e9d837c789d4bb3e4edd5cc17a7a1
Geoff Simmons <geoff@uplex.de>
2022-09-30 18:57:43 +0200

    Use the maximum value for the ADC clock divider.

--- src/main.c
+++ src/main.c
@@ -20,8 +20,11 @@
 #include "handlers.h"
 
 #define ADC_TEMP_CH (4)
-/* Clock divider to run the ADC at 2 Hz (48 MHz clock) */
-#define ADC_CLKDIV_2HZ (24 * 1000 * 1000)
+/*
+ * Maximum value for the ADC clock divider. Results in about 732,4 Hz, or
+ * approximately one measurement every 1 1/3 ms.
+ */
+#define ADC_MAX_CLKDIV (65535.f + 255.f/256.f)
 
 #define AP_SCAN_INTVL_MS (10 * 1000)
 
@@ -106,7 +109,7 @@ core1_main(void)
 	adc_select_input(ADC_TEMP_CH);
 	/* Write to FIFO length 1, and retain the ERR bit. */
 	adc_fifo_setup(true, false, 1, true, false);
-	adc_set_clkdiv(ADC_CLKDIV_2HZ);
+	adc_set_clkdiv(ADC_MAX_CLKDIV);
 
 	irq_set_exclusive_handler(ADC_IRQ_FIFO, temp_isr);
 	adc_irq_set_enabled(true);
